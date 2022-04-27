daaf9329a647f1493d3c2371c08f17bce9149ad8
Honeta <honeta@qq.com>
2022-04-27 15:05:08 +0800

    Extend fetch_data.sh

--- task3-gemm/fetch_data.sh
+++ task3-gemm/fetch_data.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+
 if [ ! -f data.tar.gz ]; then
+    echo '> Fetching https://honeta.site/files/data.tar.gz ...'
     curl https://honeta.site/files/data.tar.gz --output data.tar.gz
 fi
 
 if [ ! -d data ]; then
+    echo '> Unpacking data.tar.gz ...'
     tar -xzvf data.tar.gz
 fi
